b24bb72b46a2733772e27759d53a583fbaf0ba63
Harish Sikriwal <harish.s@blockcube.co>
2023-12-01 15:40:29 +0530

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <div className='bg-white px-[306px] py-[60px]'>
+            <p className='text-[50px] text-[#13305A] font-medium'>About Us</p>
+            <p className='text-[17px] text-[#878787]'>Welcome to Shipfinex FZCO, a pioneering force in transforming the shipping industry through our state-of-the-art Software as a Service (SaaS) platform, meticulously engineered on cutting-edge blockchain technology.
+            At Shipfinex, we specialize in offering Distributed Ledger Technology as a Service (DLTaaS), providing organizations with the essential infrastructure and connectivity services required to digitize complex multi-party workflows.
+            DLT, or distributed ledger technology, operates on a decentralized system where multiple nodes collaboratively store, validate, and update the ledger simultaneously. This approach eliminates the need for a central authority, minimizing the risk of a single point of failure. The result is a transparent, accountable, and highly secure record-keeping system that enhances data and transaction visibility.
+            Our DLTaaS platform enables organizations to streamline their processes by removing intermediaries and automating transactions through smart contracts. This not only reduces costs but also significantly increases operational efficiency. The benefits of our revolutionary platform extend beyond cost savings, offering improved security, transparency, and overall operational efficiency.
+            As a DLT as a Service provider, Shipfinex goes beyond offering mere infrastructure; we provide a comprehensive suite of services, including platform support, ensuring that organizations can rapidly innovate, accelerate development, and capitalize on opportunities without the burden of substantial startup and operational costs.
+            Embrace Shipfinex to unlock a new era of possibilities where your organization can experience heightened security, unparalleled transparency, and unmatched efficiency in navigating the complexities of the shipping industry. Join us in reshaping the future of maritime logistics with our innovative DLTaaS platform.
+            </p>
+        </div>
+
+        <div className='bg-[#F2F4F7] px-[306px] py-[60px]'>
+            <p className='text-[50px] text-[#13305A]'>Vision</p>
+            <p className='text-[17px] text-[#878787]'>
+             At Shipfinex, our vision is to be the foremost provider of blockchain-as-a-service solutions for the shipping industry, driving innovation and efficiency through cutting-edge technology. We envision a future where every aspect of maritime operations is seamlessly integrated, secure, and transparent, empowering ship owners and industry stakeholders to unlock their full potential.
+            </p>
+        </div>
+
+        <div className='bg-[#F2F4F7] px-[306px] py-[60px]'>
+            <p className='text-[50px] text-[#13305A]'>Mission:</p>
+            <p className='text-[17px] text-[#878787]'>
+             Our mission is to deliver comprehensive and adaptable blockchain solutions tailored for the shipping industry's B2B needs. We aim to simplify and streamline complex processes, fostering collaboration and trust among industry players. By providing the tools for ship owners to develop their own industry-specific use cases, we empower them to thrive in a rapidly evolving landscape.
+             </p>
+        </div>
+
+        <div className='bg-white px-[306px] py-[60px]'>
+            <p className='text-[50px] text-[#13305A]'>Our Expertise:</p>
+
+            <div className='text-[17px] text-[#878787]'>
+
+              <ul class="list-disc list-outside">
+              <li>
+                Blockchain Platform Development: At Shipfinex, we lead the way in crafting and maintaining a specialized blockchain platform tailored to the unique demands of the shipping industry. This platform forms the sturdy foundation for an array of applications and services that drive efficiency and transparency.
+                <br/><br/>What we provide is a Blockchain as a Service (BaaS) that refers to third-party cloud-based infrastructure and management for companies building and operating blockchain applications. BaaS allows customers to leverage cloud-based solutions to build, host, and operate their own blockchain apps and related functions on the blockchain, while the cloud-based service provider keeps the infrastructure agile and operational. This service simplifies blockchain development, saves costs of operations and maintenance, and facilitates business development.
+                <br/><br/>BaaS has gained significant traction in various industries due to its ability to provide solutions for optimization, transparency, and cost in a straightforward manner.
+                </li>
+                <li>
+                Smart Contracts: Our implementation of smart contracts is a testament to our commitment to automating and securing critical maritime processes. From cargo tracking to freight settlements and compliance management, our smart contracts revolutionize traditional workflows.
+                </li>
+                <li>
+                Data Security: Security is non-negotiable. Our robust encryption and authentication measures prioritize the integrity and confidentiality of every transaction and piece of information on our blockchain, ensuring a secure environment for your operations.
+                </li>
+                <li>
+                Integration Services: Seamless connectivity is key. Our integration services facilitate a smooth transition, allowing our platform to seamlessly mesh with other systems and software used by shipping companies, enhancing overall operational coherence.
+                </li>
+                <li>
+                Consulting and Training: More than just a solution provider, we empower our clients. Our consulting and training services equip you with the knowledge and understanding to harness the full potential of our blockchain-based SaaS solution.
+                </li>
+                <li>
+                Custom Development: Recognizing the uniqueness of each client, our custom development services enable you to tailor our SaaS platform to your specific requirements or industry use cases.
+                </li>
+          </ul>
+           </div>
+        </div>
+
+        <div className='bg-[#0068FF] px-[60px] py-[50px]'>
+        <svg xmlns="http://www.w3.org/2000/svg" width="177" height="40" viewBox="0 0 177 40" fill="none">
+          <path d="M5.16597 34.4319C4.76374 33.7103 5.03189 32.8005 5.75749 32.4084L21.3815 23.8672C22.9746 23.0044 23.9289 21.4123 23.9289 19.6084C23.9289 17.8044 22.9825 16.2201 21.3893 15.3574L5.74171 6.69067C5.01611 6.29067 4.75585 5.38087 5.15808 4.6593C5.56032 3.93773 6.4752 3.67891 7.20079 4.07891L22.8247 12.7299C25.3801 14.1182 26.9181 16.6829 26.9181 19.5927C26.9181 22.5025 25.3801 25.075 22.8169 26.4633L7.20079 35.0044C6.4752 35.4044 5.56032 35.1378 5.16597 34.4162V34.4319Z" fill="white"/>
+          <path d="M11.9645 38.1489C11.4598 38.1489 10.9708 37.898 10.6869 37.4352C10.2531 36.7293 10.474 35.8117 11.1759 35.3803L25.7036 26.5019C28.1328 25.0195 29.584 22.4391 29.584 19.5999C29.584 16.7607 28.1328 14.1882 25.7036 12.698L11.1759 3.82739C10.466 3.39601 10.2531 2.47837 10.6869 1.77248C11.1206 1.0666 12.0435 0.854838 12.7533 1.28621L27.2809 10.1646C30.6092 12.196 32.5889 15.7254 32.5889 19.6078C32.5889 23.4901 30.6013 27.0195 27.2809 29.0509L12.7533 37.9293C12.5088 38.0784 12.2406 38.1489 11.9725 38.1489H11.9645Z" fill="white"/>
+          <path d="M37.2892 19.6078C37.2892 30.4392 28.4637 39.2157 17.5718 39.2157C17.2879 39.2157 17.0119 39.2157 16.728 39.2L27.6592 32.5255C32.2021 29.749 34.9152 24.9255 34.9152 19.6157C34.9152 14.3059 32.1942 9.4902 27.6356 6.72157L16.6333 0.0235294C16.9488 0.00784314 17.2643 0 17.5798 0C28.4715 0 37.297 8.77647 37.297 19.6078H37.2892Z" fill="white"/>
+          <path d="M0 8.36047V30.8546C0 31.9997 1.23824 32.7134 2.23988 32.1487L22.1385 20.9016C23.1481 20.3291 23.1481 18.8781 22.1385 18.3056L2.23988 7.06635C1.23824 6.50165 0 7.22322 0 8.36047Z" fill="white"/>
+          <path d="M53.0476 19.9921C50.5475 19.2784 48.7256 18.4 47.5898 17.3568C46.4542 16.3137 45.8785 15.0274 45.8785 13.498C45.8785 11.7647 46.5725 10.3372 47.9685 9.20001C49.3645 8.0706 51.1705 7.49805 53.4026 7.49805C54.9247 7.49805 56.2734 7.78824 57.4643 8.37648C58.6552 8.95687 59.578 9.76471 60.2326 10.7921C60.8872 11.8196 61.2106 12.9411 61.2106 14.1568H58.2766C58.2766 12.8235 57.8507 11.7725 56.999 11.0117C56.1472 10.2509 54.9484 9.86663 53.4026 9.86663C51.9671 9.86663 50.8393 10.1804 50.0348 10.8156C49.2303 11.4431 48.8282 12.3215 48.8282 13.4431C48.8282 14.3451 49.2067 15.098 49.9717 15.7176C50.7368 16.3372 52.0381 16.9019 53.8757 17.4196C55.7134 17.9372 57.1489 18.5019 58.1899 19.1215C59.2309 19.7411 59.996 20.4627 60.5007 21.2862C61.0055 22.1098 61.25 23.0823 61.25 24.2039C61.25 25.9843 60.5481 27.4117 59.1521 28.4862C57.7561 29.5607 55.8869 30.0941 53.5446 30.0941C52.0223 30.0941 50.6105 29.8039 49.2935 29.2235C47.9763 28.6431 46.959 27.8509 46.2492 26.847C45.5393 25.8431 45.1765 24.698 45.1765 23.4196H48.1104C48.1104 24.7451 48.6073 25.796 49.5932 26.5725C50.5791 27.3411 51.8961 27.7254 53.5524 27.7254C55.0903 27.7254 56.2734 27.4117 57.0936 26.7921C57.9139 26.1647 58.324 25.3176 58.324 24.2431C58.324 23.1686 57.9454 22.3294 57.1883 21.7411C56.4311 21.1529 55.0509 20.5725 53.0555 20L53.0476 19.9921Z" fill="white"/>
+          <path d="M67.3546 15.4274C68.6007 13.9059 70.2176 13.1451 72.213 13.1451C75.6832 13.1451 77.442 15.0902 77.4656 18.9882V29.7882H74.6579V18.9725C74.65 17.7961 74.3739 16.9255 73.8455 16.3608C73.3171 15.7961 72.489 15.5137 71.3612 15.5137C70.4463 15.5137 69.6497 15.7569 68.9635 16.2353C68.2774 16.7216 67.7411 17.349 67.3546 18.1412V29.7882H64.5469V6.58826H67.3546V15.4274Z" fill="white"/>
+          <path d="M81.6929 9.11373C81.6929 8.65883 81.8349 8.27452 82.1109 7.96863C82.387 7.65491 82.8049 7.49805 83.3491 7.49805C83.8934 7.49805 84.3114 7.65491 84.5953 7.96863C84.8792 8.28236 85.0212 8.66667 85.0212 9.11373C85.0212 9.56079 84.8792 9.94506 84.5953 10.2431C84.3114 10.549 83.8934 10.698 83.3491 10.698C82.8049 10.698 82.387 10.549 82.1109 10.2431C81.8349 9.93721 81.6929 9.56079 81.6929 9.11373ZM84.7293 29.7961H81.9216V13.4509H84.7293V29.7961Z" fill="white"/>
+          <path d="M103.114 21.803C103.114 24.2893 102.538 26.2971 101.394 27.8187C100.251 29.3402 98.697 30.101 96.7491 30.101C94.801 30.101 93.1842 29.4736 92.0407 28.2108V36.0775H89.2329V13.45H91.7961L91.9302 15.2618C93.0738 13.85 94.6669 13.1442 96.7018 13.1442C98.7362 13.1442 100.235 13.8814 101.386 15.3638C102.538 16.8461 103.114 18.901 103.114 21.5442V21.803ZM100.306 21.4893C100.306 19.6461 99.9117 18.1951 99.1235 17.1206C98.3343 16.054 97.2459 15.5206 95.8737 15.5206C94.1701 15.5206 92.8924 16.2736 92.0485 17.7716V25.5834C92.8924 27.0736 94.178 27.8187 95.9053 27.8187C97.2539 27.8187 98.3264 27.2853 99.1157 26.2265C99.9039 25.1677 100.306 23.5834 100.306 21.4893Z" fill="white"/>
+          <path d="M107.626 29.7961V15.6079H105.031V13.451H107.626V12.0628C107.626 10.2432 108.145 8.82356 109.194 7.80395C110.243 6.78434 111.727 6.27454 113.635 6.27454C114.771 6.27454 116.206 6.58042 117.933 7.19218L117.46 9.54512C116.19 9.04316 114.991 8.79218 113.848 8.79218C112.641 8.79218 111.774 9.05885 111.245 9.60003C110.717 10.1412 110.449 10.949 110.449 12.0236V13.4432H113.809V15.6H110.449V29.7883H107.626V29.7961ZM119.337 29.7961H116.514V13.451H119.337V29.7961Z" fill="white"/>
+          <path d="M126.569 13.451L126.664 15.5059C127.918 13.9373 129.558 13.153 131.585 13.153C135.056 13.153 136.814 15.0981 136.838 18.9961V29.7961H134.03V18.9804C134.023 17.8039 133.746 16.9333 133.218 16.3686C132.689 15.8039 131.861 15.5216 130.733 15.5216C129.819 15.5216 129.022 15.7647 128.335 16.2432C127.65 16.7294 127.114 17.3569 126.727 18.149V29.7961H123.919V13.451H126.578H126.569Z" fill="white"/>
+          <path d="M147.896 30.0941C145.671 30.0941 143.858 29.3647 142.462 27.9137C141.065 26.4628 140.364 24.5098 140.364 22.0784V21.5686C140.364 19.9451 140.679 18.502 141.294 17.2235C141.917 15.953 142.785 14.949 143.905 14.2353C145.024 13.5216 146.239 13.153 147.541 13.153C149.678 13.153 151.342 13.851 152.525 15.2549C153.708 16.651 154.3 18.6588 154.3 21.2706V22.4314H143.164C143.203 24.0392 143.676 25.3412 144.583 26.3373C145.49 27.3333 146.641 27.8275 148.037 27.8275C149.031 27.8275 149.867 27.6235 150.562 27.2235C151.248 26.8235 151.855 26.2902 152.367 25.6235L154.087 26.949C152.707 29.051 150.64 30.1098 147.887 30.1098L147.896 30.0941ZM147.549 15.4431C146.413 15.4431 145.459 15.851 144.693 16.6745C143.92 17.4981 143.447 18.6431 143.265 20.1255H151.5V19.9137C151.42 18.4941 151.034 17.3961 150.348 16.6118C149.663 15.8275 148.731 15.4431 147.557 15.4431H147.549Z" fill="white"/>
+          <path d="M163.071 19.4197L166.714 13.451H169.995L164.617 21.5295L170.161 29.7961H166.912L163.11 23.6785L159.309 29.7961H156.043L161.587 21.5295L156.209 13.451H159.458L163.055 19.4197H163.071Z" fill="white"/>
+        </svg>
+
+        <div className='flex justify-between items-center text-white mt-14'>
+            <p>Shipfinex © [2023]. All Rights Reserved.</p>
+
+            <p>Privacy Policy | Terms of Service | Legal Notice</p>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
